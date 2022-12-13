4abffe955d53a62209af68d985081fb0c76f3592
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-12-13 18:27:25 +0200

    add more field names and dialog initial pane reactive code

--- src/main/java/lt/tiem625/docbuild/components/selectableitemdialog/SelectableItemDialogController.java
+++ src/main/java/lt/tiem625/docbuild/components/selectableitemdialog/SelectableItemDialogController.java
@@ -1,8 +1,55 @@
 package lt.tiem625.docbuild.components.selectableitemdialog;
 
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import lt.tiem625.docbuild.ViewableEntity;
 
-public class SelectableItemDialogController<T extends ViewableEntity> {
+import java.net.URL;
+import java.util.HashSet;
+import java.util.ResourceBundle;
+import java.util.Set;
 
+public class SelectableItemDialogController<T extends ViewableEntity> implements Initializable {
 
+    @FXML
+    private Label changingEntityLabel;
+
+    @FXML
+    private TextField searchItemTextField;
+
+    @FXML
+    private ListView<T> suggestionsListView;
+
+    private Set<? extends T> suggestions;
+    private final Property<T> prevValueProperty = new SimpleObjectProperty<>(null);
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        bindLabelToPrevValue();
+        bindTextFieldToPrevValue();
+//        suggestionsListView.itemsProperty()
+
+        setDialogData(null, null);
+    }
+
+    private void bindTextFieldToPrevValue() {
+        searchItemTextField.textProperty().bind(prevValueProperty.map(ViewableEntity::asView));
+    }
+
+    private void bindLabelToPrevValue() {
+        changingEntityLabel.textProperty().bind(
+                prevValueProperty.map(passedVal -> String.format("Changing set value '%s'...", passedVal.asView()))
+        );
+    }
+
+    public void setDialogData(T prevValue, Set<? extends T> suggestions) {
+        this.prevValueProperty.setValue(prevValue);
+        this.suggestions = suggestions != null ? new HashSet<>(suggestions) : Set.of();
+    }
 }
