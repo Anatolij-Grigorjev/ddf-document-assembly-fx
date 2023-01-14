9999ea56d9e1e6f8225d831da5c26cb6b1a6cc35
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2023-01-14 10:56:08 +0200

    add more missing element bindings and initializers

--- src/main/java/lt/tiem625/docbuild/components/structuresflow/StructureMapsFlowViewController.java
+++ src/main/java/lt/tiem625/docbuild/components/structuresflow/StructureMapsFlowViewController.java
@@ -1,7 +1,12 @@
 package lt.tiem625.docbuild.components.structuresflow;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
@@ -18,26 +23,44 @@ public class StructureMapsFlowViewController implements Initializable {
 
     @FXML
     private ListView<StructuresMapFlow> lvStructureMaps;
+    private final ListProperty<StructuresMapFlow> structureMaps =
+            new SimpleListProperty<>(FXCollections.observableArrayList());
+
+    @FXML
+    private Button btnRemoveStructuresMapping;
+
+    @FXML
+    private Button btnFinalizeFlow;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        lvStructureMaps.setItems(structureMaps);
+        btnFinalizeFlow.disableProperty().bind(structureMaps.emptyProperty());
+        btnRemoveStructuresMapping.disableProperty()
+                .bind(Bindings.or(
+                        structureMaps.emptyProperty(),
+                        lvStructureMaps.getSelectionModel().selectedItemProperty().isNotNull()
+                ));
+    }
 
     @FXML
     private void onFinalizeFlowClicked() {
 
     }
+
     @FXML
     private void onRemoveStructuresMappingClicked() {
 
     }
+
     @FXML
     private void onAddStructuresMappingClicked() {
 
     }
+
     @FXML
     private void onEditApplicationsContextClicked() {
-
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
 
     }
 }
